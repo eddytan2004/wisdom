87cbb39a105aff7c25eaeaba7f7d867dd06d20b4
eddytan2004 <eddy_tan2004@hotmail.com>
2022-06-10 16:04:03 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function () {
-        document.addEventListener("deviceready", this.onDeviceReady, false);
+        $(document).addEventListener("deviceready", this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
@@ -16,37 +16,29 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
         //callNativeFunction();
-        document.getElementById("capture-btn").addEventListener(
-            "click",
-            function () {
-                var element = document.getElementById("root");
-                html2canvas(element, {
-                    onrendered: function (canvas) {
-                        var dataURL = canvas.toDataURL();
-                        document.getElementById("screenshotimg").src = dataURL;
-                    },
-                });
-            },
-            false
-        );
+        $("#capture-btn").click(function () {
+            var element = $("#root");
+            html2canvas(element, {
+                onrendered: function (canvas) {
+                    var dataURL = canvas.toDataURL();
+                    $("#screenshotimg").src = dataURL;
+                },
+            });
+        }, false);
 
-        document.getElementById("email-btn").addEventListener(
-            "click",
-            function () {
-                var imgData = document.getElementById("screenshotimg").src;
-                cordova.plugins.email.isAvailable(function (isAvailable) {
-                    cordova.plugins.email.open({
-                        to: "",
-                        cc: "",
-                        bcc: [],
-                        subject: "",
-                        //attachments: imgData,
-                        body: '<img src="' + imgData + '">',
-                    });
+        $("#email-btn").click(function () {
+            var imgData = $("#screenshotimg").src;
+            cordova.plugins.email.isAvailable(function (isAvailable) {
+                cordova.plugins.email.open({
+                    to: "",
+                    cc: "",
+                    bcc: [],
+                    subject: "",
+                    //attachments: imgData,
+                    body: '<img src="' + imgData + '">',
                 });
-            },
-            false
-        );
+            });
+        }, false);
     },
     // Update DOM on a Received Event
     receivedEvent: function (id) {},
